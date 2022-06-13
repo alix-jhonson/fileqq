9d34bdac5682431bf73819dfb68e731633cd26b5
rana haseeb <alix@yahoo.com>
2022-06-13 15:58:15 +0500

    code

---
+++ monday13.rb
@@ -0,0 +1,17 @@
+
+marks = 50
+case marks
+  
+ when 0..32
+  puts "You fail!"
+  
+ when 33..40
+  puts "You got C grade!"
+  
+ when 41..60
+  puts "You got B grade!"
+  
+ else
+ puts  "You got A grade!"
+   
+ end
